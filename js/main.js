b8c714c3da6fcf853adf65d0cb99de2cc608dee0
cohen479 <72747553+cohen479@users.noreply.github.com>
2021-07-17 10:05:19 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }
+    else{
+       return results[1] || 'No Activity';
+    }
+}
+
 dpi_x = $('#dpidiv').first().outerWidth();
 dpi_y = $('#dpidiv').first().outerHeight();
-$('#info_span').html('dpi: ' + dpi_x + ' by ' + dpi_y);
-var dataString = '" & Activities::Period One & "';
-document.getElementById('activity_span').innerHTML = dataString;
-var qrcode = new QRCode('qrcode', {width: 96, height: 96, text: dataString});
+
+$('#info_span').html('' + window.location.href + '. dpi: ' + dpi_x + ' by ' + dpi_y);
+
+var a1 =  decodeURIComponent($.urlParam('a1')); //'" & Activities::Period One & "';
+
+$('#activity_span').innerHTML = a1;
+
+var qrcode = new QRCode('qrcode', {width: 96, height: 96, text: a1});
 
 var qr = new QRious({
         element: document.getElementById('qr'),
         size: 96, 
-        value: dataString
+        value: a1
     });

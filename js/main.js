f6a11a6cd253f470413538e15ac5773a88367fe4
cohen479 <72747553+cohen479@users.noreply.github.com>
2021-07-17 10:19:06 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,27 @@ $.urlParam = function(name){
     }
 }
 
+// var getUrlParameter = function getUrlParameter(sParam) {
+//     var sPageURL = window.location.search.substring(1),
+//         sURLVariables = sPageURL.split('&'),
+//         sParameterName,
+//         i;
+
+//     for (i = 0; i < sURLVariables.length; i++) {
+//         sParameterName = sURLVariables[i].split('=');
+
+//         if (sParameterName[0] === sParam) {
+//             return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+//         }
+//     }
+//     return false;
+// };
+// And this is how you can use this function assuming the URL is,
+// http://dummy.com/?technology=jquery&blog=jquerybyexample.
+
+// var tech = getUrlParameter('technology');
+// var blog = getUrlParameter('blog');
+
 dpi_x = $('#dpidiv').first().outerWidth();
 dpi_y = $('#dpidiv').first().outerHeight();
 

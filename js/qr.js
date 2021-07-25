8d27aba2a7834c2ba6660e8fbfd9e3da4945494b
cohen479 <72747553+cohen479@users.noreply.github.com>
2021-07-25 13:10:53 -0400

    Create qr.js

---
+++ js/qr.js
@@ -0,0 +1,24 @@
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }
+    else{
+       return results[1] || 'No Activity';
+    }
+}
+
+dpi_x = $('#dpidiv').first().outerWidth();
+dpi_y = $('#dpidiv').first().outerHeight();
+
+var a1 =  decodeURIComponent($.urlParam('contents')); //'" & Activities::Period One & "';
+
+if (a1 && a1 !== 'No Activity') { 
+    var qr1 = new QRCode('qr1', {width: 70, height: 70, text: a1}); 
+} 
+
+// var qr = new QRious({
+//         element: $('#qr')[0], //$('#qr').first()
+//         size: 96, 
+//         value: a1
+//     });
